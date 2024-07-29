9cbf1e01f301d011571a14658e42db75be53ee1b
khandillu96 <khandillu96@gmail.com>
2024-07-29 13:52:03 +0530

    all responsive styling done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,7 @@
-/* Author: 
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navbar.classList.toggle("active");
+})
